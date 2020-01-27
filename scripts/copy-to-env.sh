05919cfc68a935986d1c351ed1858c985917bdf9
Tim Fall <tfall@grubhub.com>
2020-01-27 15:52:40 -0800

    Some stuff @grampelberg made up

--- scripts/copy-to-env.sh
+++ scripts/copy-to-env.sh
@@ -3,8 +3,8 @@ github_changelog_generator $@
 
 # If CHANGELOG.md exits, copy it to an env var
 if [[ -e 'CHANGELOG.md' ]]; then
-  export CHANGELOG="$(cat CHANGELOG.md)"
+  export CHANGELOG="$(cat CHANGELOG.md | sed ':a;N;$!ba;s/%/%25/g' | sed ':a;N;$!ba;s/\n/%0A/g' | sed ':a;N;$!ba;s/\r/%0D/g)"
 fi
 
 echo "$CHANGELOG"
-echo ::set-output name=changelog::'${CHANGELOG}'
+echo ::set-output name=changelog::"${CHANGELOG}"
