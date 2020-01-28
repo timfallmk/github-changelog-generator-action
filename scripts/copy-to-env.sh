18d686eeaa4aaec5ab061b4ea4303a2cb8967185
Tim Fall <tfall@grubhub.com>
2020-01-27 16:01:15 -0800

    Missing quote

--- scripts/copy-to-env.sh
+++ scripts/copy-to-env.sh
@@ -3,7 +3,7 @@ github_changelog_generator $@
 
 # If CHANGELOG.md exits, copy it to an env var
 if [[ -e 'CHANGELOG.md' ]]; then
-  export CHANGELOG="$(cat CHANGELOG.md | sed ':a;N;$!ba;s/%/%25/g' | sed ':a;N;$!ba;s/\n/%0A/g' | sed ':a;N;$!ba;s/\r/%0D/g)"
+  export CHANGELOG="$(cat CHANGELOG.md | sed ':a;N;$!ba;s/%/%25/g' | sed ':a;N;$!ba;s/\n/%0A/g' | sed ':a;N;$!ba;s/\r/%0D/g')"
 fi
 
 echo "$CHANGELOG"
